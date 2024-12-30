924e9fd7919f92a22861bccd47e9babe4f051481
SpiderTheus <matemora80@gmail.com>
2024-12-30 10:59:16 -0300

    alterando genero

--- src/main/java/aplicacao/models/LivroModel.java
+++ src/main/java/aplicacao/models/LivroModel.java
@@ -1,81 +1,69 @@
 package aplicacao.models;
-
-/*
- - isbn: String   |
-| - titulo: String |
-| - autor: String  |
-| - genero: String |
-| - ano: int       |
-| - disponivel: boolean |
-| - emprestimos: int    |
- */
-
-
+import java.util.List;
 import java.util.UUID;
 
-public class LivroModel {
+    public class LivroModel {
     //randomUUID()
     private UUID livroId;
     private String isbn;
     private String autor;
-    private String genero;
+    private List<String> genero;
     private String data_lancamento;
     private boolean disponivel;
 
-    public LivroModel(String isbn, String autor, String genero, String data_lancamento, boolean disponivel) {
+   public LivroModel(String isbn, String autor, List<String> genero, String data_lancamento, boolean disponivel) {
         this.livroId = UUID.randomUUID();
         this.isbn = isbn;
         this.autor = autor;
         this.genero = genero;
         this.data_lancamento = data_lancamento;
         this.disponivel = disponivel;
-    }
+         }
 
-    public UUID getLivroId() {
-        return livroId;
-    }
+           public UUID getLivroId() {
+                return livroId;
+            }
 
-    public void setLivroId(UUID livroId) {
-        this.livroId = livroId;
-    }
+            public void setLivroId(UUID livroId) {
+                this.livroId = livroId;
+            }
 
-    public String getIsbn() {
-        return isbn;
-    }
+           public String getIsbn() {
+               return isbn;
+            }
 
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
+          public void setIsbn(String isbn) {
+               this.isbn = isbn;
+           }
 
-    public String getAutor() {
-        return autor;
-    }
+           public String getAutor() {
+               return autor;
+          }
 
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
+          public void setAutor(String autor) {
+                this.autor = autor;
+            }
 
-    public String getGenero() {
-        return genero;
-    }
+            public List<String> getGenero() {
+                return genero;
+            }
 
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
+           public void setGenero() {
+                this.genero = genero;
+            }
 
-    public String getData_lancamento() {
-        return data_lancamento;
-    }
+            public String getData_lancamento() {
+                return data_lancamento;
+            }
 
-    public void setData_lancamento(String data_lancamento) {
-        this.data_lancamento = data_lancamento;
-    }
+            public void setData_lancamento(String data_lancamento) {
+                this.data_lancamento = data_lancamento;
+           }
 
-    public boolean isDisponivel() {
-        return disponivel;
-    }
-
-    public void setDisponivel(boolean disponivel) {
-        this.disponivel = disponivel;
-    }
+         public boolean isDisponivel() {
+                return disponivel;
+            }
+     public void setDisponivel(boolean disponivel) {
+              this.disponivel = disponivel;
+            }
 }
